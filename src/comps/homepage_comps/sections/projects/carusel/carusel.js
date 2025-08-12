05e6e1cb61fc57251be05196522e4939d1876f1f
MirShukhman <miriysha@gmail.com>
2025-08-12 17:57:14 -0500

    prj section grid

--- src/comps/homepage_comps/sections/projects/carusel/carusel.js
+++ src/comps/homepage_comps/sections/projects/carusel/carusel.js
@@ -4,18 +4,41 @@ import { useState } from 'react'
 import './carusel.css'
 import { TfiAngleLeft, TfiAngleRight } from "react-icons/tfi";
 
+const projects = ["PROJ 1", "PROJ 2", "PROJ 3", "PROJ 4", "PROJ 5"]
+
 export default function Carousel() {
     const [current, setCurrent] = useState(0)
 
+    const moveLeft = () => {
+        setCurrent(prev => (prev - 1 + projects.length) % projects.length)
+    }
+
+    const moveRight = () => {
+        setCurrent(prev => (prev + 1) % projects.length)
+    }
+
+    // Helper to wrap indexes
+    const mod = (n, m) => (n + m) % m
+
     return (
         <div className="carousel-container">
-            <div className="carousel-btn" id="carousel-btn-l"><TfiAngleLeft /></div>
-            <div className='carusel-inner'>
-                <div className='proj-on-carusel' id='p-l'>PROJ 1</div>
-                <div className='proj-on-carusel' id='current'>PROJ 2</div>
-                <div className='proj-on-carusel' id='p-r'>PROJ 3</div>
+            <div className="carousel-btn" onClick={moveLeft}><TfiAngleLeft /></div>
+            <div className="carusel-inner">
+                {projects.map((proj, index) => {
+                    let className = "proj-on-carusel"
+                    if (index === current) className += " center"
+                    else if (index === mod(current - 1, projects.length)) className += " left"
+                    else if (index === mod(current + 1, projects.length)) className += " right"
+                    else className += " hidden"
+
+                    return (
+                        <div key={index} className={className}>
+                            {proj}
+                        </div>
+                    )
+                })}
             </div>
-            <div className="carousel-btn" id="carousel-btn-r"><TfiAngleRight /></div>
+            <div className="carousel-btn" onClick={moveRight}><TfiAngleRight /></div>
         </div>
     )
 }
