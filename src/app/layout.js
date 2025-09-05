25629b58fc76de9ac7229e6473bada19bff16e3a
MirShukhman <miriysha@gmail.com>
2025-09-05 15:46:01 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
 import "./layout.css"
 
+export const metadata = {
+  title: "Miriam Adam",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
